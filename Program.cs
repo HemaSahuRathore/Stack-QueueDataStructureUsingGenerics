7e9c370616bc58f33807282947e5a9e57a3c1d95
Hema <hemasahu019@gmail.com>
2022-08-06 22:19:10 +0530

    Added Welcome message to main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StackNQueueUsingGenerics
+{ /// <summary>
+  /// Stack and Queue Data Structure using Generics
+  /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack & Queue Data Structure"); //display welcome message
+        }
+    }
+}
