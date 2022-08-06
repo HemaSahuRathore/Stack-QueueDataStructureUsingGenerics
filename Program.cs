de4021cdc41ebae99aa1fa8f6999283cf01997c4
Hema <hemasahu019@gmail.com>
2022-08-06 22:32:47 +0530

    Added UC2 code to peek and pop till stack becomes empty

--- Program.cs
+++ Program.cs
@@ -11,9 +11,10 @@
             Console.WriteLine("Welcome to Stack & Queue Data Structure"); //display welcome msg
             do
             {
-                Console.WriteLine("\nPlease select the Options from Below: ");
+                Console.WriteLine("\nPlease select the Options from Below Use case to execute: ");
                 Console.WriteLine("1.Create a Stack of 56->30->70");
-                Console.Write("2.Exit  ");
+                Console.WriteLine("2.Peak and Pop from the Stack till it is empty 56->30->70");
+                Console.Write("3.Exit  ");
                 option = Convert.ToInt32(Console.ReadLine());
 
                 switch (option)
@@ -25,9 +26,17 @@
                         stack.Push(70);
                         stack.Display();
                         break;
+                    case 2:
+                        Stack<int> stackObj = new Stack<int>();
+                        stackObj.Push(56);
+                        stackObj.Push(30);
+                        stackObj.Push(70);
+                        Console.WriteLine("Is stack Empty : " + stackObj.IsEmpty());
+                        Console.WriteLine();
+                        break;
 
                 }
-            } while (option != 2);
+            } while (option != 3);
 
         }
     }
--- Stack.cs
+++ Stack.cs
@@ -36,5 +36,44 @@ namespace StackNQueueUsingGenerics
             }
 
         }
+
+        //display top most data
+        public void peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty!! No elemt is present.");
+                return;
+            }
+            else
+                Console.WriteLine("The top most element in stack is " + this.top.data);
+        }
+
+        //remove top most element
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty!! No element is present");
+                return;
+            }
+            else
+            {
+                T deleteNode = top.data;
+                this.top = top.next; //assiging top to next element
+                Console.WriteLine("{0} is deleted from the top", deleteNode);
+            }
+        }
+
+        //return true if stack is empty else false
+        public bool IsEmpty()
+        {
+            while (this.top != null)
+            {
+                peek();
+                Pop();
+            }
+            return true;
+        }
     }
 }
